9d218d0dce981a1e6c9fba8e5c40bba6dc00d27b
AmeerHamza <ameerhamza7171@gmail.com>
2022-01-26 14:53:58 +0500

    first commit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
-const express = require('express')
+// const express = require('express')
 
-const app = express();
-const port = process.env.PORT || 30001; 
+// const app = express();
+const port = process.env.PORT || 30002; 
 const hostname = '127.0.0.2';
 
-app.listen(port,()=>{
-    console.log(`Server running at http://{hostname}:${port}/`);
-    //apis.connect()
-});
+console.log(`app.js is opened`);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.listen(port,()=>{
+//     console.log(`Server running at http://{hostname}:${port}/`);
+//     //apis.connect()
+// });
+
+// app.get('/', (req, res) => res.send('Hello World!'))
